552583696c26a012c2d0c4290e914569ca152cab
PeruDayani <perudayani@berkeley.edu>
2022-02-21 01:40:08 -0500

    working app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Main from './components/main.js'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{'marginBottom': '0px'}}> Whoooooo </h1>
+      <h4> A win is a win :)</h4>
+      <Main />
     </div>
   );
 }
---
+++ src/components/hardcoded_teams.js
@@ -0,0 +1,29 @@
+export const TEAM_PERU = [
+    'Antetokounmpo',
+    'James',
+    'Morant',
+    'Jokic',
+    'Wiggins',
+    'Booker',
+    'Towns',
+    'Middleton',
+    'Garland',
+    'Paul',
+    'Mitchell',
+    'Murray'
+  ]
+  
+export const TEAM_HAYDEN = [
+    'Curry',
+    'Young',
+    'Embiid',
+    'Tatum',
+    'DeRozan',
+    'Doncic',
+    'LaVine',
+    'Allen',
+    'Gobert',
+    'Ball',
+    'VanVleet',
+    'Butler'
+  ]
